dad40b7d5e7d3412297f86cbfaf78077178b5882
Sine997 <103395877+Sine997@users.noreply.github.com>
2022-04-19 13:03:23 +0800

    Add files via upload

---
+++ 111.js
@@ -0,0 +1,18 @@
+var a = prompt("Please enter the first number：","");
+			var b = prompt("Please enter the second number：","");
+			a = parseInt(a);
+			b = parseInt(b);
+			alert(a + b);
+/*English name ：Joey
+   Name PinYin ： Chen Jiu
+
+This is the summation site. It is possible to calculate the sum of two numbers, especially if there is no input or if the input is not a number, an error will be reported automatically
+
+
+  Comments can be made
+
+  Statement variety, variety is rich
+
+  Doing dynamic websites is particularly flexible
+
+*/
